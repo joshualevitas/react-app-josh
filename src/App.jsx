c857dc38ad91e8499f31765a33a538bb009e6459
Josh Levitas <joshlevitas@dhcp-10-102-34-246.wireless.northwestern.private>
2022-10-02 11:05:34 -0500

    schedule start

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,13 @@ import { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+
+const schedule = {
+  title: "CS Courses for 2018-2019"
+};
+
+
+
 const App = () => {
   const [count, setCount] = useState(0);
 
