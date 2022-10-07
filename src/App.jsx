651a78fac5ec190d4c0d8c3d3496da57d15b7a3f
Josh Levitas <joshlevitas@dhcp-10-102-34-246.wireless.northwestern.private>
2022-10-07 11:10:05 -0500

    fetch done

--- src/App.jsx
+++ src/App.jsx
@@ -3,39 +3,32 @@ import logo from './logo.svg';
 import './App.css';
 import Banner from './components/banner';
 import CourseList from './components/courseList';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { useJsonQuery } from './utilities/fetch.js';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap-icons/font/bootstrap-icons.css';
 
 
-const schedule = {
-  "title": "CS Courses for 2018-2019",
-  "courses": {
-    "F101" : {
-      "term": "Fall",
-      "number": "101",
-      "meets" : "MWF 11:00-11:50",
-      "title" : "Computer Science: Concepts, Philosophy, and Connections"
-    },
-    "F110" : {
-      "term": "Fall",
-      "number": "110",
-      "meets" : "MWF 10:00-10:50",
-      "title" : "Intro Programming for non-majors"
-    },
-    "S313" : {
-      "term": "Spring",
-      "number": "313",
-      "meets" : "TuTh 15:30-16:50",
-      "title" : "Tangible Interaction Design and Learning"
-    },
-    "S314" : {
-      "term": "Spring",
-      "number": "314",
-      "meets" : "TuTh 9:30-10:50",
-      "title" : "Tech & Human Interaction"
-    }
-  }
-};
+const Main = () => {
+  const [data, isLoading, error] = useJsonQuery('https://courses.cs.northwestern.edu/394/guides/data/cs-courses.php');
+
+  if (error) return <h1>Error loading user data: {`${error}`}</h1>;
+  if (isLoading) return <h1>Loading user data...</h1>;
+  if (!data) return <h1>No user data found</h1>;
+
+  return (
+    <div>
+      <h1 className="title">{data.title}</h1>
+      <CourseList courses = {data.courses} />
+    </div>
+
+
+  );
+}
+
+const queryClient = new QueryClient();
+
+
 
 
 
@@ -43,20 +36,12 @@ const App = () => {
   const [count, setCount] = useState(0);
 
   return (
-    <div className="App">
-      <div className="title">
-        <Banner title={schedule.title}/>
-      </div>
-      <div className="container">
-        <CourseList courses={schedule.courses}/>
-      </div>
-
-    
-        
-        
-  
-    </div>
-  );
+      <QueryClientProvider client = {queryClient}>
+        <div className='container'>
+          <Main/>
+        </div>
+      </QueryClientProvider>
+    );
 };
 
 export default App;
